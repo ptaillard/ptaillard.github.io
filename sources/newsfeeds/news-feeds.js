45ed22ba7d75aa4a2eb80e88a0a11cbb84713b3c
pierre.taillard <pierre_taillard@yahoo.fr>
2015-12-13 16:01:56 +0100

    use dashboard resource to NewsFeeds service

--- sources/newsfeeds/news-feeds.js
+++ sources/newsfeeds/news-feeds.js
@@ -1,16 +1,19 @@
+/* global _ */
 (function() {
     'use strict'
 
     angular.module('newsFeeds')
         .service('NewsFeeds', NewsFeeds);
 
-    function NewsFeeds() {
+    function NewsFeeds(DashboardResource) {
         var service = this;
 
         service.datas = {};
 
         function load() {
-            service.datas.news = ['john doe'];
+            DashboardResource.then(function(data) {
+                _.extend(service.datas, data.feed_items.feed_items);
+            });
         };
         load();
     };
